48b7c27ba40c94de3f918dab35a210489a8cb16e
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-11-14 13:13:23 +0530

    login request exception handling

--- UserService/src/main/java/org/example/Services/AuthService.java
+++ UserService/src/main/java/org/example/Services/AuthService.java
@@ -1,6 +1,7 @@
 package org.example.Services;
 
 import org.example.DTO.UserJson;
+import org.example.Exceptions.UnauthorizedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -58,6 +59,7 @@ public class AuthService{
         return logClient()
                 .flatMap(token -> {
                     UserJson jsonobj = new UserJson(firstName, lastName,email, password);
+
                     return webClient.post()
                             .uri(addUserEndpoint)
                             .header("Authorization", "Bearer " + token)
@@ -82,8 +84,16 @@ public class AuthService{
                         .with("password", password)
                 )
                 .retrieve()
+                .onStatus(status -> status == HttpStatus.UNAUTHORIZED, response -> Mono.error(new UnauthorizedException("Invalid credentials")))
                 .bodyToMono(Map.class)
-                .map(responseMap -> (String) responseMap.get("access_token"));
+                .map(responseMap -> (String) responseMap.get("access_token"))
+                .onErrorResume(ex -> {
+                    if (ex instanceof UnauthorizedException) {
+                        return Mono.error(ex);  // Pass 401 Unauthorized error to the controller
+                    } else {
+                        return Mono.error(new RuntimeException("Unexpected error")); // Handle other errors
+                    }
+                });
     }
 
 }
