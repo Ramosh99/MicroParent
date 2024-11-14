a8861d97b9403949779c2d6ad5fe2a565ee7c09c
Rashmina Amarasinghe <shasheenrashmina@gmail.com>
2024-11-14 19:26:18 +0530

    Exceptions

--- OrderService/src/main/java/org/example/Controller/ShoppingCartController.java
+++ OrderService/src/main/java/org/example/Controller/ShoppingCartController.java
@@ -19,25 +19,39 @@ public class ShoppingCartController {
     private final ShoppingCartServices shoppingCartServices;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void AddToCart(@RequestBody AddToCartDto addToCartDto) {
-        shoppingCartServices.addToCart(addToCartDto);
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<String> AddToCart(@RequestBody AddToCartDto addToCartDto) {
+        try {
+            shoppingCartServices.addToCart(addToCartDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Item added to the cart");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
     }
 
     @GetMapping
-    @ResponseStatus(HttpStatus.FOUND)
-    public ShoppingCart GetCart(@RequestBody GetCartDto getCartDto) {
-        return shoppingCartServices.getCart(getCartDto);
+    //@ResponseStatus(HttpStatus.FOUND)
+    public ResponseEntity<?> GetCart(@RequestBody GetCartDto getCartDto) {
+        try {
+            ShoppingCart shoppingCart= shoppingCartServices.getCart(getCartDto);
+            return ResponseEntity.status(HttpStatus.FOUND).body(shoppingCart);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
     }
 
     @DeleteMapping
     public ResponseEntity<String> RemoveItem(@RequestBody RemoveItemDto removeItemDto){
-        boolean isRemoved = shoppingCartServices.removeItemFromCart(removeItemDto);
+        try {
+            boolean isRemoved = shoppingCartServices.removeItemFromCart(removeItemDto);
 
-        if (isRemoved) {
-            return ResponseEntity.ok("Item removed from cart successfully");
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Item not found in cart or cart does not exist");
+            if (isRemoved) {
+                return ResponseEntity.ok("Item removed from cart successfully");
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Item not found in cart or cart does not exist");
+            }
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
 
